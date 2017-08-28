48d8067e68819ea0510d8bd8223de9a830b25e03
DCJ <david@dcj.codes>
2017-08-28 18:56:40 +0100

    TEASER PAGE | Update social links

--- src/components/navigation/SocialNavigation.js
+++ src/components/navigation/SocialNavigation.js
@@ -9,8 +9,8 @@ const SocialNavigation = () => {
       		<h3 className="navigation__title">Main Navigation</h3>
       		<ul>
       			<li><a href="https://github.com/iamDCJ/">GitHub</a></li>
-      			<li><a href="twitter.com/iamdcj">Twitter</a></li>
-      			<li><a href="instagram.com/iam.dcj">Instagram</a></li>
+      			<li><a href="https://twitter.com/iamdcj">Twitter</a></li>
+      			<li><a href="https://instagram.com/iam.dcj">Instagram</a></li>
             <li><a href="https://www.linkedin.com/in/david-christian-jones-73063758/">Linkedin</a></li>
       		</ul>
       	</nav>
