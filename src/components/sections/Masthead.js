fed9abcea445bb7f29db1fdb0a5ff2b91d628f31
DCJ <david@dcj.codes>
2017-08-28 20:39:51 +0100

    TEASER | Try alternate branding for splash page

--- src/components/sections/Masthead.js
+++ src/components/sections/Masthead.js
@@ -11,10 +11,10 @@ const Masthead = () => {
    			<div className="masthead__inner">
 	    		<h2 className="[ masthead__title ]">Hello</h2>
 
-	    		<p className="[ masthead__subtitle ]">I am David Christian Jones, a front end developer with over five years experience building application user-interfaces for small-to-global companies and non-profits.
+	    		<p className="[ masthead__subtitle ]">I am David Christian Jones, a Manchester&ndash;based front&ndash;end developer current building application user interfaces for small-to-global companies over at <a href="http://www.building-blocks.com" className="btn  btn--style-a">Building Blocks</a>.
 				</p>
 
-	    		<a href="mailto:david@dcj.codes" className="btn">Get in touch</a>
+	    		<a href="mailto:david@dcj.codes" className="btn  btn--style-b">Get in touch &#8594;</a>
     		</div>
 
         <SocialNavigation />
