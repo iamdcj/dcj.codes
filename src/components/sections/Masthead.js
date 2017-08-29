5e12ee3d394e93747aaaa83b5247853c708af7d6
DCJ <david@dcj.codes>
2017-08-29 08:18:44 +0100

    TEASER PAGE | Invert color scheme

--- src/components/sections/Masthead.js
+++ src/components/sections/Masthead.js
@@ -11,10 +11,10 @@ const Masthead = () => {
    			<div className="masthead__inner">
 	    		<h2 className="[ masthead__title ]">Hello</h2>
 
-	    		<p className="[ masthead__subtitle ]">I am David Christian Jones, a Manchester&ndash;based front&ndash;end developer current building application user interfaces for small-to-global companies over at <a href="http://www.building-blocks.com" className="btn  btn--style-a">Building Blocks</a>.
-				</p>
+	    	  <p className="[ masthead__subtitle ]">I am David Christian Jones, a Manchester&ndash;based front&ndash;end developer currently building application <abbr title="User Interfaces">UIs</abbr> for global companies over at <a href="http://www.building-blocks.com" className="btn  btn--style-a">Building Blocks (UK)</a></p>
 
 	    		<a href="mailto:david@dcj.codes" className="btn  btn--style-b">Get in touch &#8594;</a>
+
     		</div>
    		</section>
     );
