49e253d60f66814c66d69b2cd3a32ff80784a6d8
DCJ <david@dcj.codes>
2017-09-02 08:46:10 +0100

    TEASER PAGE | Small tweaks to main grid

--- src/components/sections/Masthead.js
+++ src/components/sections/Masthead.js
@@ -8,7 +8,7 @@ const Masthead = () => {
     return (
    		<section className="[ masthead ]  grid">
 
-   			<div className="masthead__inner  grid__item  tab--two-thirds">
+   			<div className="masthead__inner  grid__item  desk--two-thirds">
 	    		<h2 className="[ masthead__title ]">Hello</h2>
 
 	    		<p className="[ masthead__subtitle ]">I am David Christian Jones, a Manchester&ndash;based front&ndash;end developer currently building application <abbr title="User Interfaces">UIs</abbr> for global companies over at <a href="http://www.building-blocks.com" className="btn  btn--style-a">Building Blocks (UK)</a></p>
