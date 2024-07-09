5f8d2535e7e258e0432ad2b351dbf30d4737003b
David Jones <david.jones@deptagency.com>
2024-07-09 12:45:29 -0400

    delete

--- src/app/cms/page.tsx
+++ src/app/cms/page.tsx
@@ -1,12 +1,23 @@
-import Link from "next/link";
+import Articles from "@/components/Articles";
 
-export default function Home() {
-  return (
-    <section>
-      Current Posts <Link href="/cms/new">Create New Post +</Link>
-      {
-        // map over current posts
-      }
-    </section>
-  );
+async function getPosts() {
+  try {
+    const res = await fetch("http://localhost:3001/api/blog");
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch posts, received status ${res.status}`);
+    }
+
+    const posts = await res.json();
+
+    return posts;
+  } catch (error) {
+    return [];
+  }
+}
+
+export default async function CMS() {
+  const posts: any[] = await getPosts();
+
+  return posts && posts.length > 0 && <Articles isAdmin posts={posts} />;
 }
--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,11 +1,44 @@
-export default function Articles({ posts }) {
+'use client';
+
+export default function Articles({
+  posts,
+  isAdmin = false,
+}: {
+  posts: any[];
+  isAdmin: boolean;
+}) {
+
+  const handleDelete = async (id: any) => {
+    console.log(id);
+    
+    try {
+      const res = await fetch(`http://localhost:3001/api/blog/${id}`, {
+        method: 'DELETE',
+        credentials: 'include'
+      });
+  
+      if (!res.ok) {
+        throw new Error(`Failed to fetch posts, received status ${res.status}`);
+      }
+  
+      const posts = await res.json();
+  
+      return posts;
+    } catch (error) {
+      return [];
+    }
+  }
+
   return (
-    <section className="section section--approach">
+    <section className="section section--posts">
       <header className="section__header">
-        <h2 className="section__heading">About</h2>
-        <div className="section__subtitle">
+        <h2 className="section__heading">Thoughts</h2>
+        <div className="section__content">
           {posts.map((post: any) => (
             <article>
+              {
+                isAdmin && <button onClick={() => handleDelete(post._id)}>Delete</button>
+              }
               <h3>{post.title}</h3>
               <div dangerouslySetInnerHTML={{ __html: post.content }} />
             </article>
